e4bfb78ccfd5659a1f1fff3f3016b0b89d537915
user.name <kitpipat.0647@gmail.com>
2025-01-04 21:45:37 +0700

    feat: init packages

---
+++ packages/common/src/utils.ts
@@ -0,0 +1,27 @@
+import { UnauthorizedException } from '@nestjs/common'
+import { FastifyRequest } from 'fastify'
+
+export type Context = FastifyRequest & {
+  raw: {
+    user: {
+      id: string
+      email: string
+      phoneNumber: string
+      firstName: string
+      lastName: string
+      gender: string
+      dateOfBirth: string
+      role: 'USER' | 'MASSAGER' | 'PROPERTY_OWNER' | 'ADMIN'
+      isEmailVerified: boolean
+    } | null
+  }
+}
+
+export const getUserFromContext = (ctx: Context) => {
+  const user = ctx.raw.user
+  if (!user) {
+    throw new UnauthorizedException('กรุณาเข้าสู่ระบบ')
+  }
+
+  return user
+}
