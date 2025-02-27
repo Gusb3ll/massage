654f3087769a9c5405bf692ab91777f721d53ce6
user.name <kitpipat.0647@gmail.com>
2025-02-27 22:29:13 +0700

    feat: init auth & fetchers

---
+++ apps/client/src/services/user/types.ts
@@ -0,0 +1,44 @@
+export type RegisterArgs = {
+  email: string
+  password: string
+  firstName: string
+  lastName: string
+  phoneNumber: string
+  gender: 'MALE' | 'FEMALE'
+  dateOfBirth: string
+  role: 'USER' | 'MASSAGER' | 'PROPERTY_OWNER'
+}
+
+export type LoginArgs = {
+  email: string
+  password: string
+}
+
+export type VerifyEmailArgs = {
+  token: string
+}
+
+export type User = {
+  id: string
+  email: string
+  phoneNumber: string
+  firstName: string
+  lastName: string
+  gender: string
+  dateOfBirth: string
+  role: 'USER' | 'MASSAGER' | 'PROPERTY_OWNER' | 'ADMIN'
+  isEmailVerified: boolean
+}
+
+export type UpdateUserArgs = {
+  firstName?: string
+  lastName?: string
+  phoneNumber?: string
+  dateOfBirth?: string
+  gender?: 'MALE' | 'FEMALE'
+}
+
+export type UpdatePasswordArgs = {
+  oldPassword: string
+  newPassword: string
+}
