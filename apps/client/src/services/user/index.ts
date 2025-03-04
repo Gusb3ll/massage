cc3c479b62cd37fe8e67d0dd5c1e9f92e35173be
user.name <kitpipat.0647@gmail.com>
2025-03-04 22:43:38 +0700

    fix: remove unused

--- apps/client/src/services/user/index.ts
+++ apps/client/src/services/user/index.ts
@@ -10,23 +10,11 @@ import {
   User,
 } from './types'
 
-// export const register = async (args: RegisterArgs) => {
-//   const res = await fetchers.Post(
-//     `${ENDPOINT}/user/public/register`,
-//     { data: args },
-//   )
-//   if (res.statusCode >= HttpStatus.BAD_REQUEST) {
-//     throw new Error(res.message)
-//   }
-
-//   return res.data
-// }
 export const register = async (args: RegisterArgs) => {
   const res = await fetchers.Post<{ accessToken: string }>(
     `${ENDPOINT}/user/public/register`,
     { data: args },
   )
-
   if (res.statusCode >= HttpStatus.BAD_REQUEST) {
     throw new Error(res.message)
   }
