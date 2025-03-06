b26a591ee64b188a98ca8c6af5c20051a0dd9b02
user.name <kitpipat.0647@gmail.com>
2025-03-07 02:28:20 +0700

    feat: reviews

--- apps/server/src/api/review/review.module.ts
+++ apps/server/src/api/review/review.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common'
 
 import { ReviewInternalController } from './internal/internal.controller'
 import { ReviewInternalService } from './internal/internal.service'
+import { ReviewPublicController } from './public/public.controller'
+import { ReviewPublicService } from './public/public.service'
 
 @Module({
-  controllers: [ReviewInternalController],
-  providers: [ReviewInternalService],
+  controllers: [ReviewInternalController, ReviewPublicController],
+  providers: [ReviewInternalService, ReviewPublicService],
 })
 export class ReviewModule {}
