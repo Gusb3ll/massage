02539bbdd103ffaf2f9971efcfe0f836f6905913
Jeerasak Daradas <164070808+Jelegummykub@users.noreply.github.com>
2025-03-10 15:35:50 +0700

    Feat(Client) : Massager Profile page (#17)

--- apps/client/src/services/massager/index.ts
+++ apps/client/src/services/massager/index.ts
@@ -3,21 +3,21 @@ import { ENDPOINT, HttpStatus, fetchers } from '@/utils'
 import { Massager } from './types'
 
 export const getMassager = async (id: string) => {
-  const res = await fetchers.Get<Massager>(`${ENDPOINT}/massager/public/${id}`)
-  if (res.statusCode >= HttpStatus.BAD_REQUEST) {
-    throw new Error(res.message)
-  }
+    const res = await fetchers.Get<Massager>(`${ENDPOINT}/massager/public/${id}`)
+    if (res.statusCode >= HttpStatus.BAD_REQUEST) {
+        throw new Error(res.message)
+    }
 
-  return res.data as Massager
+    return res.data as Massager
 }
 
 export const getMassagers = async () => {
-  const res = await fetchers.Get<Massager[]>(`${ENDPOINT}/massager/public/list`)
-  if (res.statusCode >= HttpStatus.BAD_REQUEST) {
-    throw new Error(res.message)
-  }
+    const res = await fetchers.Get<Massager[]>(`${ENDPOINT}/massager/public/list`)
+    if (res.statusCode >= HttpStatus.BAD_REQUEST) {
+        throw new Error(res.message)
+    }
 
-  return res.data as Massager[]
+    return res.data as Massager[]
 }
 
 export * from './types'
