108efa4f28296d28b728080c753d1acf98d9787d
user.name <kitpipat.0647@gmail.com>
2025-04-01 01:27:37 +0700

    feat: massager vac & cert

--- apps/client/src/scenes/Dashboard/User/Massager/Modal/License.tsx
+++ apps/client/src/scenes/Dashboard/User/Massager/Modal/License.tsx
@@ -11,26 +11,30 @@ type LicenseModalProps = {
 const LicenseModal: React.FC<LicenseModalProps> = ({ massager }) => {
   return (
     <dialog ref={licenseModalRef} className="modal">
-      <div className="modal-box">
+      <div className="modal-box overflow-hidden">
         <form method="dialog">
           <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
             ✕
           </button>
         </form>
         <h3 className="text-lg font-bold">License Certificates</h3>
-        {massager.certificates.length > 0 ? (
-          <>
-            <img
-              src={massager.certificates[0] ?? '/default-avatar.png'}
-              alt="License Certificates"
-              className="h-[300px] w-full items-center rounded-lg object-cover"
-            />
-          </>
-        ) : (
-          <div className="mt-2 flex h-[200px] items-center justify-center rounded-lg border border-dashed border-gray-300">
-            Massager has no certificates
-          </div>
-        )}
+        <div className="flex max-h-[800px] flex-col gap-4 overflow-auto p-2">
+          {massager.certificates.length > 0 ? (
+            massager.certificates.map((c, i) => (
+              // eslint-disable-next-line @next/next/no-img-element
+              <img
+                key={`cert-${i}`}
+                src={c}
+                alt={`cert-${i}`}
+                className="h-[300px] w-full items-center rounded-lg object-cover"
+              />
+            ))
+          ) : (
+            <div className="mt-2 flex h-[200px] items-center justify-center rounded-lg border border-dashed border-gray-300">
+              Massager has no certificates
+            </div>
+          )}
+        </div>
       </div>
       <form method="dialog" className="modal-backdrop">
         <button>close</button>
--- apps/client/src/scenes/Dashboard/User/Massager/Modal/Vaccine.tsx
+++ apps/client/src/scenes/Dashboard/User/Massager/Modal/Vaccine.tsx
@@ -12,28 +12,30 @@ const VaccineModal: React.FC<VaccineModalProps> = ({ massager }) => {
   return (
     <>
       <dialog ref={vaccineModalRef} className="modal">
-        <div className="modal-box">
+        <div className="modal-box overflow-hidden">
           <form method="dialog">
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
               ✕
             </button>
           </form>
           <h3 className="text-lg font-bold">Vaccine Certificates</h3>
-          {massager.vaccineCertificates.length > 0 ? (
-            massager.vaccineCertificates.map((c, i) => (
-              // eslint-disable-next-line @next/next/no-img-element
-              <img
-                key={`vacc-${i}`}
-                src={c}
-                alt={`vacc-${i}`}
-                className="h-[300px] w-full items-center rounded-lg object-cover"
-              />
-            ))
-          ) : (
-            <div className="mt-2 flex h-[200px] items-center justify-center rounded-lg border border-dashed border-gray-300">
-              Massager has no certificates
-            </div>
-          )}
+          <div className="flex max-h-[800px] flex-col gap-4 overflow-auto p-2">
+            {massager.vaccineCertificates.length > 0 ? (
+              massager.vaccineCertificates.map((c, i) => (
+                // eslint-disable-next-line @next/next/no-img-element
+                <img
+                  key={`vacc-${i}`}
+                  src={c}
+                  alt={`vacc-${i}`}
+                  className="h-[300px] w-full items-center rounded-lg object-cover"
+                />
+              ))
+            ) : (
+              <div className="mt-2 flex h-[200px] items-center justify-center rounded-lg border border-dashed border-gray-300">
+                Massager has no certificates
+              </div>
+            )}
+          </div>
         </div>
         <form method="dialog" className="modal-backdrop">
           <button>close</button>
